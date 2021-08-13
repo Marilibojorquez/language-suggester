5194d579a4f2c69c0281d5bcda90a36068b60940
Marili Bojorquez <marilibojorquez@yahoo.com>
2021-08-13 16:48:36 -0700

    Add output of survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
-const startButton = document.getElementById('survey-btn')
-
-
-
-function startSurvey() {
-  console.log('Started')
-  startButton.classList.add('hide')
-}
-
-function questionOne() {
-
-}
-if(condition is library) {
-  //do this cool stuff
-}else if(condition is true) {
-  //do this other cool sruff 
-}else{
-  //default cool stuff
-}
+$(document).ready(function() {
+  $("button#container").click(function() {
+    $("body").addClass("question")
+  }
+});
