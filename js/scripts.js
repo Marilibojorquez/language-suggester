4cc9f6c905c3ad7125efcfadcb89fb970c1cbff2
Marilibojorquez <88338816+Marilibojorquez@users.noreply.github.com>
2021-08-18 10:55:37 -0700

    Add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-document.getElementsById("container")
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    const q1 = parseInt($("input#scale").val());
+    const q2 = parseInt($("input#scale2").val());
+    const q3 = parseInt($("input#scale3").val());
+    const q4 = parseInt($("input#scale4").val());
+    const q5 = parseInt($("input#scale5").val());
+  
+
+
+    let suggestion = ["Javascript", "Go", "Swift"]
+    
+    console.log(suggestion)  
+
+    $("#language").text(scale);
+    $("#suggestion").show();
+    });
+  });
+
+// Suggest language 
+  var suggestion = ['Go', 'Javascript', 'Swift', 'C#',]
+
+  function newSuggestion() {
+  var suggestLanguage = ("Javascript")
+}
+
+
+
